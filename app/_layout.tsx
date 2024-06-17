c730afd6979cfba60f16fb3ecf4f46ed3bf2911e
julia <juliasagazcorrea2004@gmail.com>
2024-06-17 16:39:56 -0300

    viagens-app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,72 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
-  );
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { FontAwesome, Ionicons, MaterialIcons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+export default function Layout() {
+ return (
+ <Tabs
+ screenOptions={{
+ tabBarStyle: {
+ backgroundColor: Colors.bgColor,
+ borderTopWidth: 0,
+ padding: 0,
+ },
+ tabBarShowLabel: false,
+ tabBarActiveTintColor: Colors.black,
+ tabBarInactiveTintColor: "#999",
+ }}
+ >
+ <Tabs.Screen
+ name="index"
+ options={{
+ tabBarIcon: ({ color }) => (
+ <Ionicons name="compass" size={28} color={color} />
+ ),
+ }}
+ />
+ <Tabs.Screen
+ name="category"
+ options={{
+ tabBarIcon: ({ color }) => (
+ <MaterialIcons name="space-dashboard" size={28} color={color} />
+ ),
+ }}
+ />
+ <Tabs.Screen
+ name="search"
+ options={{
+ tabBarIcon: ({ color }) => (
+ <View
+ style={{
+ backgroundColor: Colors.primaryColor,
+ paddingHorizontal: 16,
+ paddingVertical: 12,
+ borderRadius: 10,
+ height: 50,
+ }}
+ >
+ <Ionicons name="search-outline" size={24} color={Colors.white} />
+ </View>
+ ),
+ }}
+ />
+ <Tabs.Screen
+ name="bookmarks"
+ options={{
+ tabBarIcon: ({ color }) => (
+ <Ionicons name="bookmark" size={28} color={color} />
+ ),
+ }}
+ />
+ <Tabs.Screen
+ name="profile"
+ options={{
+ tabBarIcon: ({ color }) => (
+ <FontAwesome name="user" size={28} color={color} />
+ ),
+ }}
+ />
+ </Tabs>
+ );
 }
